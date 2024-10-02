29d456bd944c6a878d3c9af7c69ff61ff8660ca5
Šimon Macek <simon.macek@proficio.cz>
2024-10-02 15:26:36 +0200

    migrate events from another project

--- wp-content/themes/kontrapunkt/templates/event/event.php
+++ wp-content/themes/kontrapunkt/templates/event/event.php
@@ -66,93 +66,71 @@ register_post_type( "event", $args );
 
 $event_fields = new FieldsBuilder('Událost');
 $event_fields
-  ->addText('type', [
-    'label' => 'Druh akce',
-    'required' => 0,
-    'placeholder' => 'Zadejte krátký název',
-    'maxlength' => 40,
-    'wpml_cf_preferences' => 2,
-  ])
-  ->addWysiwyg('description', [
-    'label' => 'Popis události',
-    'required' => 1,
-    'media_upload' => FALSE,
-  ])
-  ->addText('location', [
-    'label' => 'Místo',
-    'required' => 1,
-    'placeholder' => 'Zadejte název',
-    'maxlength' => 40,
-    'wpml_cf_preferences' => 2,
-  ])
-  ->addTrueFalse('has_location_map', [
-    'label' => 'Ukázat pozici na mapě',
-    'required' => 0,
-    'default_value' => 0,
-    'ui' => 1,
-    'ui_on_text' => 'Ano',
-    'ui_off_text' => 'Ne',
-    'wpml_cf_preferences' => 1,
-  ])
-  ->addGoogleMap('location_map', [
-    'label' => 'Místo na mapě',
-    'instructions' => 'Označte místo kde se akce děje',
-    'required' => 0,
-    'center_lat' => '50.0755',
-    'center_lng' => '14.4378',
-    'zoom' => '10',
-    'wpml_cf_preferences' => 1,
-  ])->conditional('has_location_map', '==', '1')
-  ->addTrueFalse('is_date_single', [
-    'label' => 'Akce je jednodenní',
-    'required' => 0,
-    'default_value' => 1,
-    'ui' => 1,
-    'ui_on_text' => 'Ano',
-    'ui_off_text' => 'Ne',
-    'wpml_cf_preferences' => 1,
-  ])
-  ->addDatePicker('date_single', [
-    'label' => 'Datum akce',
-    'instructions' => 'Vyplňte datum kdy akce začíná.',
-    'required' => 1,
-    'display_format' => 'd/m/Y',
-    'return_format' => 'j. n. Y',
-    'first_day' => 1,
-    'wpml_cf_preferences' => 1,
-  ])->conditional('is_date_single', '==', '1')
-  ->addTimePicker('time_start', [
-    'label' => 'Začátek akce',
-    'instructions' => 'Vyplňte čas kdy akce začíná',
-    'required' => 1,
-    'display_format' => 'g:i a',
-    'return_format' => 'g:i a',
-  ])->conditional('is_date_single', '==', '1')
-  ->addTimePicker('time_end', [
-    'label' => 'Konec akce',
-    'instructions' => 'Vyplňte čas kdy akce končí',
-    'required' => 1,
-    'display_format' => 'g:i a',
-    'return_format' => 'g:i a',
-  ])->conditional('is_date_single', '==', '1')
-  ->addDateTimePicker('date_time_start', [
-    'label' => 'Akce začíná',
-    'instructions' => 'Vyplňte datum a čas zahájení akce.',
-    'required' => 1,
-    'display_format' => 'd/m/Y',
-    'return_format' => 'j. n. Y',
-    'first_day' => 1,
-    'wpml_cf_preferences' => 1,
-  ])->conditional('is_date_single', '==', '0')
-  ->addDateTimePicker('date_time_end', [
-    'label' => 'Akce končí',
-    'instructions' => 'Vyplňte datum a čas ukončení akce.',
-    'required' => 1,
-    'display_format' => 'd/m/Y',
-    'return_format' => 'j. n. Y',
-    'first_day' => 1,
-    'wpml_cf_preferences' => 1,
-  ])->conditional('is_date_single', '==', '0');
+->addImage('image', [
+  'label' => 'Obrázek',
+  'required' => 1,
+  'wpml_cf_preferences' => 1,
+])
+->addWysiwyg('perex', [
+  'label' => 'Popis události',
+  'required' => 1,
+  'media_upload' => FALSE,
+  'show_in_grpahql' => TRUE,
+])
+->addText('location', [
+  'label' => 'Místo',
+  'required' => 1,
+  'placeholder' => 'Zadejte název',
+  'maxlength' => 40,
+  'wpml_cf_preferences' => 2,
+])
+->addText('price', [
+  'label' => 'Cena',
+  'required' => 1,
+  'placeholder' => 'Zadejte cenu',
+  'maxlength' => 40,
+  'wpml_cf_preferences' => 2,
+])
+->addDatePicker('date_start', [
+  'label' => 'Datum začátku akce',
+  'instructions' => 'Vyplňte datum kdy akce začíná.',
+  'required' => 1,
+  'display_format' => 'd. m. Y',
+  'return_format' => 'Y-m-d',
+  'first_day' => 1,
+  'wpml_cf_preferences' => 1,
+])
+->addDatePicker('date_end', [
+  'label' => 'Datum konce akce',
+  'instructions' => 'Vyplňte datum kdy akce končí.',
+  'required' => 0,
+  'display_format' => 'd. m. Y',
+  'return_format' => 'Y-m-d',
+  'first_day' => 1,
+  'wpml_cf_preferences' => 1,
+])
+->addTimePicker('time_start', [
+  'label' => 'Začátek akce',
+  'instructions' => 'Vyplňte čas kdy akce začíná',
+  'required' => 0,
+  'display_format' => 'H:i',
+  'return_format' => 'H:i',
+  'wpml_cf_preferences' => 1,
+])
+->addTimePicker('time_end', [
+  'label' => 'Konec akce',
+  'instructions' => 'Vyplňte čas kdy akce končí',
+  'required' => 0,
+  'display_format' => 'H:i',
+  'return_format' => 'H:i',
+  'wpml_cf_preferences' => 1,
+])
+->addText('duration', [
+  'label' => 'Doba trvání',
+  'instructions' => 'Vyplňte délku trvání akce',
+  'required' => 0,
+  'wpml_cf_preferences' => 2,
+])
 
 $event_fields->setLocation('post_type', '==', 'event');
 
