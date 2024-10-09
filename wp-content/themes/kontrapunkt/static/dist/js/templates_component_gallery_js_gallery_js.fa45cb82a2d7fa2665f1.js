0e483e98e4d9f3a6e88a767d8a969367d38a6166
Šimon Macek <simon.macek@proficio.cz>
2024-10-09 21:45:33 +0200

    hide language-switcher and cleanup gallery script

--- wp-content/themes/kontrapunkt/static/dist/js/templates_component_gallery_js_gallery_js.fa45cb82a2d7fa2665f1.js
+++ wp-content/themes/kontrapunkt/static/dist/js/templates_component_gallery_js_gallery_js.fa45cb82a2d7fa2665f1.js
@@ -13,7 +13,7 @@ __webpack_require__.r(__webpack_exports__);
 
 const elArray = document.getElementsByClassName("js-gallery");
 for (let i = 0; i < elArray.length; i++) {
-  var gallery = (0,lightgallery__WEBPACK_IMPORTED_MODULE_0__["default"])(elArray[i], {
+  const gallery = (0,lightgallery__WEBPACK_IMPORTED_MODULE_0__["default"])(elArray[i], {
     selector: ".js-lightbox"
   });
 }
@@ -21,4 +21,4 @@ for (let i = 0; i < elArray.length; i++) {
 /***/ })
 
 }]);
-//# sourceMappingURL=templates_component_gallery_js_gallery_js.2ad5e7f1e55c41bf4904.js.map
+//# sourceMappingURL=templates_component_gallery_js_gallery_js.fa45cb82a2d7fa2665f1.js.map
--- wp-content/themes/kontrapunkt/static/templates/component/gallery/js/gallery.js
+++ wp-content/themes/kontrapunkt/static/templates/component/gallery/js/gallery.js
@@ -4,7 +4,7 @@ import lightGallery from "lightgallery";
 const elArray = document.getElementsByClassName("js-gallery");
 
 for (let i = 0; i < elArray.length; i++) {
-  var gallery = lightGallery(elArray[i], {
+  const gallery = lightGallery(elArray[i], {
     selector: ".js-lightbox",
   });
 }
